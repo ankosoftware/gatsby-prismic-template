21ec3e06c95514001d70d900495a9ad107c7958b
avovchuk <a.vovchuk@gmail.com>
2020-06-03 14:07:28 +0700

    buttons and icons

--- src/components/common/rich-text.component.jsx
+++ src/components/common/rich-text.component.jsx
@@ -5,26 +5,58 @@ import Prism from "prismjs"
 
 export const RichText = ({ render, className }) => {
   const element = useRef(null)
+  const highlightCode = () => {
+    const codeBlocks = render.filter(item => {
+      return item.type === "preformatted" && item.text.indexOf("```") === 0
+    })
+    codeBlocks.forEach(codeBlock => {
+      const index = render.indexOf(codeBlock)
+      const match = codeBlock.text.match(/```([^\s\f\r\n]+)/)
+      let lang = "clike"
+      if (match && match.length) {
+        lang = match[1]
+      }
+      const text = codeBlock.text.replace(/```([^\s\f\r\n]+)?/gim, "")
+      const html = Prism.highlight(text, Prism.languages[lang], lang)
+      const code = document.createElement("code")
+      code.innerHTML = html
+      const parent = element.current.childNodes[index]
+      parent.innerHTML = ""
+      parent.appendChild(code)
+    })
+  }
+  const applyImages = () => {
+    const links = element.current.getElementsByTagName("a")
+    Array.from(links).forEach(link => {
+      const parent = link.parentNode
+      if (link.textContent.trim() === "[image]") {
+        const image = document.createElement("img")
+        image.className = "rich-text-inline-image"
+        image.src = link.href
+        parent.insertBefore(image, link)
+        parent.removeChild(link)
+      } else if (link.textContent.trim().indexOf("[button") === 0) {
+        const confStr = link.textContent
+        const textContentMatch = confStr.match(/{([^}]+)}/)
+        if (textContentMatch && textContentMatch.length > 1) {
+          link.textContent = textContentMatch[1]
+        }
+        const classNameMatch = confStr.match(/\[button:([^\s}]+)/)
+        let className = "btn"
+        if (classNameMatch && classNameMatch.length > 1) {
+          const btnProps = classNameMatch[1].split(":")
+          btnProps.forEach(prop => {
+            className += " btn-" + prop
+          })
+        }
+        link.className = className
+      }
+    })
+  }
   useEffect(() => {
     if (element && element.current && render) {
-      const codeBlocks = render.filter(item => {
-        return item.type === "preformatted" && item.text.indexOf("```") === 0
-      })
-      codeBlocks.forEach(codeBlock => {
-        const index = render.indexOf(codeBlock)
-        const match = codeBlock.text.match(/```([^\s\f\r\n]+)/)
-        let lang = "clike"
-        if (match && match.length) {
-          lang = match[1]
-        }
-        const text = codeBlock.text.replace(/```([^\s\f\r\n]+)?/gim, "")
-        const html = Prism.highlight(text, Prism.languages[lang], lang)
-        const code = document.createElement("code")
-        code.innerHTML = html
-        const parent = element.current.childNodes[index]
-        parent.innerHTML = ""
-        parent.appendChild(code)
-      })
+      highlightCode()
+      applyImages()
     }
   })
   if (typeof render === "string") {
